accffb56416d58ce938b95797025f0b0d5aee73e
Muhammad Sadaqat <m.sadaqat8686@gmail.com>
2025-11-21 14:54:40 +0100

    Add files via upload

---
+++ install_tree2gd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Creat Tree2gd Environment
+conda create -n Tree2gd python=3.8 -y
+
+# Activate Tree2gd environment
+conda activate Tree2gd
+
+# Configure conda channels
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda config --add channels defaults
+
+conda install -y -c conda-forge r-base=4.2.3 -y
+pip install --upgrade pip
+pip install Tree2gd
+
+conda install -y -c bioconda diamond muscle iqtree mcl -y
+
+conda deactivate
+echo "Installation completed."
+echo "Use: conda activate Tree2gd"
+
+# Test Tree2gd
+Tree2gd -h
+
